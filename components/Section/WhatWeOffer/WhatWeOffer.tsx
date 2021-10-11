03ed68315ff671d76d1e6617235ba3760778817d
ngthuongdoan <ngthuongdoan@gmail.com>
2021-10-11 17:49:30 +0700

    feat: :lipstick: create what we offer section

--- components/Section/WhatWeOffer/WhatWeOffer.tsx
+++ components/Section/WhatWeOffer/WhatWeOffer.tsx
@@ -1,8 +1,22 @@
+import { SERVICES } from '@common/services';
+import ServiceCard from '@components/Service/Service';
 import classes from './WhatWeOffer.module.scss';
 interface Props {}
 
 const WhatWeOffer = (props: Props) => {
-  return <div></div>;
+  return (
+    <section className={classes['what-we-offer']}>
+      <div className={classes['what-we-offer__headline']}>
+        <h2>OUR SERVICES</h2>
+        <h1>What We Offer</h1>
+      </div>
+      <div className={classes['what-we-offer__services']}>
+        {SERVICES.map((service, index) => (
+          <ServiceCard key={index} {...service}></ServiceCard>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default WhatWeOffer;
---
+++ components/Service/Service.tsx
@@ -0,0 +1,17 @@
+import Button from '@components/UI/Button/Button';
+import { Service } from '@model/Service';
+import classes from './Service.module.scss';
+interface Props {}
+
+const ServiceCard: React.VFC<Props & Service> = (props) => {
+  return (
+    <div className={classes.card}>
+      <img src={props.image} className={classes['card__image']} />
+      <h3 className={classes['card__title']}>{props.title}</h3>
+      <p className={classes['card__description']}>{props.description}</p>
+      <Button className={classes['card__button']}>SEE MORE</Button>
+    </div>
+  );
+};
+
+export default ServiceCard;
